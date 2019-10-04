418d37bbb7b423b43e1eaf5e14f4962896c80bbe
brenton <>
2019-10-04 14:01:17 -0400

    Restructured Vec2

--- scripts/math/vec2.js
+++ scripts/math/vec2.js
@@ -26,6 +26,26 @@ var math;
         Vec2.Distance = function (P1, P2) {
             return Math.floor(Math.sqrt(Math.pow(P2.x - P1.x, 2) + Math.pow(P2.y - P1.y, 2)));
         };
+        Vec2.Difference = function (a, b) {
+            return new math.Vec2(a.x - b.x, a.y - b.y);
+        };
+        Object.defineProperty(Vec2.prototype, "Magnitude", {
+            get: function () {
+                return Math.sqrt((this.x * this.x) + (this.y * this.y));
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(Vec2.prototype, "Normalized", {
+            get: function () {
+                var mag = this.Magnitude;
+                if (mag != 0) {
+                    return new Vec2(this.x / mag, this.y / mag);
+                }
+            },
+            enumerable: true,
+            configurable: true
+        });
         return Vec2;
     }(createjs.Point));
     math.Vec2 = Vec2;
--- scripts/math/vec2.ts
+++ scripts/math/vec2.ts
@@ -9,5 +9,20 @@ module math {
         public static Distance(P1:Vec2, P2:Vec2): number {
             return Math.floor(Math.sqrt(Math.pow(P2.x - P1.x, 2) + Math.pow(P2.y - P1.y, 2)));
         }
+
+        public static Difference(a: Vec2, b: Vec2): Vec2 {
+            return new math.Vec2(a.x - b.x, a.y - b.y);
+        }
+
+        public get Magnitude(): number {
+            return Math.sqrt((this.x * this.x) + (this.y * this.y));
+        }
+
+        public get Normalized(): Vec2 {
+            let mag = this.Magnitude;
+            if (mag != 0) {
+                return new Vec2(this.x / mag, this.y / mag);
+            }
+        }
     }
 }
